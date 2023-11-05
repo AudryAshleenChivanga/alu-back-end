301a8d270f9b76e6f74b503d764fb15ac02999cd
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-05 09:00:24 +0000

    file 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,20 +8,21 @@ import csv
 import requests
 import sys
 
+
 def fetch_employee_tasks(employee_id):
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
         employee_id)
     user_response = requests.get(user_url)
     user_data = user_response.json()
     employee_name = user_data.get(
-        "name", "Unknown")  # Change 'username' to 'name' to match the first script
+        "name", "Unknown")
 
     t_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     todo_response = requests.get(t_url)
     todos = todo_response.json()
 
     # Create a CSV file and write the tasks data
-    with open(f"{employee_id}_tasks.csv", "w", newline='') as csvfile:
+    with open(f"{employee_id}.csv", "w", newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
             writer.writerow(
