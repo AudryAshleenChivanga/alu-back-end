fb7451ba84fc59b35759ebccac2f6aca3bc596a8
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-05 05:56:27 +0000

    file 2

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
 """
-main_1.py: Script to validate the functionality of the script from task #0.
+Script to gather data from the JSONPlaceholder API.
+
+Fetches and displays an employee's TODO list progress and exports to a CSV file.
 """
 
-from your_script_name import fetch_employee_data
 
-def validate_employee_data(user_id):
-    """Validate the correct retrieval of user ID and username."""
-    data = fetch_employee_data(user_id)
+import csv
+import requests
+import sys
 
-    if data:
-        user_id_retrieved = data.get('user_id')
-        username_retrieved = data.get('username')
 
-        if user_id_retrieved == user_id and username_retrieved:
-            print("User ID and Username: OK")
-        else:
-            print("Username: Incorrect")
-    else:
-        print("Username: Incorrect")
+BASE_URL = 'https://jsonplaceholder.typicode.com'
 
+def fetch_employee_data(user_id):
+    """Fetch and display the employee's TODO list progress and write to CSV."""
+    user = requests.get(f'{BASE_URL}/users/{user_id}').json()
+    todos = requests.get(f'{BASE_URL}/todos?userId={user_id}').json()
+
+    # Write to CSV file
+    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            csv_writer.writerow(
+                [user_id, user.get(
+                    'name'), task.get('completed'), task.get('title')])
+
+    # Display progress as before
+    completed_tasks = [task for task in todos if task.get('completed')]
+    total_tasks = len(todos)
+    completed_count = len(completed_tasks)
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            user.get('name'), completed_count, total_tasks))
+    for task in completed_tasks:
+        print("\t " + task.get('title'))
 
 if __name__ == "__main__":
-    user_id_to_test = "2"  # or whatever user ID you want to test
-    validate_employee_data(user_id_to_test)
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        user_id = sys.argv[1]
+        fetch_employee_data(user_id)
+    else:
+        print("Please provide a valid user ID as an argument.")
