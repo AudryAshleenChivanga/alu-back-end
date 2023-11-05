0214de2107725b7d9b05ca31a046d41892e08941
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-05 08:49:19 +0000

    file1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
 
 """
-This is the first file I created for the APIs
+The second file using APIs
 """
+
 import csv
 import requests
 import sys
 
-
 def fetch_employee_tasks(employee_id):
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
     user_response = requests.get(user_url)
     user_data = user_response.json()
-    employee_name = user_data.get("name", "Unknown")
+    employee_name = user_data.get(
+        "name", "Unknown")  # Change 'username' to 'name' to match the first script
 
     t_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     todo_response = requests.get(t_url)
     todos = todo_response.json()
 
-    with open(f"{employee_id}.csv", "w", newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    # Create a CSV file and write the tasks data
+    with open(f"{employee_id}_tasks.csv", "w", newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
-            taskwriter.writerow([employee_id, employee_name, task.get(
-                "completed", False), task.get("title")])
+            writer.writerow(
+                [employee_id, employee_name, task.get(
+                    "completed", False), task.get(
+                        "title")])
 
 if __name__ == "__main__":
+    # Taking input from the command line
     employee_id = sys.argv[1]
+    # Calling the function to fetch and write tasks into a CSV file
     fetch_employee_tasks(employee_id)
