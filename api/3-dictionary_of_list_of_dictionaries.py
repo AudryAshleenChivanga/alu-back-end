d742e0960412b204d5c5a31dfb6eec087851b848
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-05 10:32:12 +0000

    file3 debugged

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 
 """
-Using what you did in the task #0, extend your 
+Using what you did in the task #0, extend your
 Python script to export data in the JSON format.
 """
 
 
-import requests
 import json
+import requests
 
 def fetch_all_tasks_for_all_users():
     # Base URLs
     users_url = "https://jsonplaceholder.typicode.com/users"
     todos_url = "https://jsonplaceholder.typicode.com/todos"
-    
+
     # Fetching users and todos data
     users = requests.get(users_url).json()
     todos = requests.get(todos_url).json()
-    
+
     user_task_dict = {}
-    
+
     for user in users:
         user_id = user["id"]
         user_name = user["username"]
@@ -32,7 +32,7 @@ def fetch_all_tasks_for_all_users():
             for todo in todos if todo["userId"] == user_id
         ]
         user_task_dict[str(user_id)] = user_tasks
-    
+
     with open("todo_all_employees.json", "w") as outfile:
         json.dump(user_task_dict, outfile, indent=4)
 
