193dba15eff9c393588ededaba60e8e60ac064f2
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-05 08:34:37 +0000

    file1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-"""
-Script to gather data from the JSONPlaceholder API and export to CSV.
 
-Fetches and exports an employee's TODO list.
-"""
-
-
-import csv
 import requests
 import sys
+import csv
 
+def fetch_employee_tasks(employee_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data.get("name", "Unknown")
 
-def fetch_employee_data(user_id):
-    """Fetch and export the employee's TODO list to CSV."""
-    user = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(
-            user_id)).json()
-    todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-            user_id)).json()
+    t_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todo_response = requests.get(t_url)
+    todos = todo_response.json()
 
-    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    with open(f"{employee_id}.csv", "w", newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
-            csv_writer.writerow(
-                [user_id, user.get('name'), task.get(
-                    'completed'), task.get('title')])
-
-    print("Data exported to {}.csv".format(user_id))
+            taskwriter.writerow([employee_id, employee_name, task.get("completed", False), task.get("title")])
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        user_id = sys.argv[1]
-        fetch_employee_data(user_id)
+    employee_id = sys.argv[1]
+    fetch_employee_tasks(employee_id)
