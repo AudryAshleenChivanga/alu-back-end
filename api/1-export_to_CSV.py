9b7514e9efb8a60b26b67e1186bead3d16442d09
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-05 07:17:37 +0000

    file1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Script to gather data from the JSONPlaceholder API.
+Script to gather data from the JSONPlaceholder API and export to CSV.
 
-Fetches and displays an employee's TODO list progress and exports to a CSV file.
+Fetches and exports an employee's TODO list.
 """
 
 
@@ -11,30 +11,23 @@ import requests
 import sys
 
 
-BASE_URL = 'https://jsonplaceholder.typicode.com'
-
 def fetch_employee_data(user_id):
-    """Fetch and display the employee's TODO list progress and write to CSV."""
-    user = requests.get(f'{BASE_URL}/users/{user_id}').json()
-    todos = requests.get(f'{BASE_URL}/todos?userId={user_id}').json()
+    """Fetch and export the employee's TODO list to CSV."""
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            user_id)).json()
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            user_id)).json()
 
-    # Write to CSV file
-    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
+    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
             csv_writer.writerow(
-                [user_id, user.get(
-                    'name'), task.get('completed'), task.get('title')])
+                [user_id, user.get('name'), task.get(
+                    'completed'), task.get('title')])
 
-    # Display progress as before
-    completed_tasks = [task for task in todos if task.get('completed')]
-    total_tasks = len(todos)
-    completed_count = len(completed_tasks)
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            user.get('name'), completed_count, total_tasks))
-    for task in completed_tasks:
-        print("\t " + task.get('title'))
+    print("Data exported to {}.csv".format(user_id))
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1].isdigit():
