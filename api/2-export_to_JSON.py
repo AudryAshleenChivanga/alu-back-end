450ac8cd25223d71111b8ca9bb71002700760e36
Ashleen Chivanga <127235974+AudryAshleenChivanga@users.noreply.github.com>
2023-11-05 10:20:46 +0000

    file2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -42,6 +42,6 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: ./script_name.py employee_id")
         sys.exit(1)
-    
+        
     employee_id = sys.argv[1]
     fetch_employee_tasks(employee_id)
